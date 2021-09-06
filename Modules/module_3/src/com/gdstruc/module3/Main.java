16eb928ddde13e86d09fb49394cd5144b2fe2240
David Gutierrez <davidandrew.gutierrez@benilde.edu.ph>
2021-09-06 18:13:58 +0800

    Added Module 3

---
+++ Modules/module_3/src/com/gdstruc/module3/Main.java
@@ -0,0 +1,46 @@
+package com.gdstruc.module3;
+
+public class Main {
+
+    /* ArrayList Stack
+    public static void main(String[] args) {
+
+        ArrayStack stack = new ArrayStack(5);
+
+        stack.push(new Player(1, "aceu", 100));
+        stack.push(new Player(2, "Sinatraa", 100));
+        stack.push(new Player(3, "Subroza", 95));
+        stack.push(new Player(4, "ploo", 90));
+
+        stack.printStack();
+
+        System.out.println("\nPopping: " + stack.pop());
+        System.out.println("Peeking: " + stack.peek());
+
+        System.out.println(" ");
+
+        stack.printStack();
+    }
+     */
+
+    /* LinkedList Stack
+    public static void main(String[] args) {
+
+        LinkedStack stack = new LinkedStack();
+
+        stack.push(new Player(1, "aceu", 100));
+        stack.push(new Player(2, "Sinatraa", 100));
+        stack.push(new Player(3, "Subroza", 95));
+        stack.push(new Player(4, "ploo", 90));
+
+        stack.printStack();
+
+        System.out.println("\nPopping: " + stack.pop());
+        System.out.println("Peeking: " + stack.peek());
+
+        System.out.println(" ");
+
+        stack.printStack();
+    }
+     */
+}
